b808156269cf29117733d7ed055bb09f08be36a9
Ivan95 <130286916+Ivantanski@users.noreply.github.com>
2024-03-02 10:56:08 -0500

    Add files via upload

---
+++ mapsAndSets.js
@@ -0,0 +1,24 @@
+//1. ([1,2,3,4])
+
+// 2. "ref"
+
+// 3.  0: {Array(3) => true}
+ //   1: {Array(3) => false}
+
+//4. function hasDuplicate(arr) {
+  //  return new Set(arr).size !== arr.length;
+// }
+
+//5. function vowelCount(str) {
+  //  const vowels = 'aeiou';
+    //const result = new Map();
+
+    //for (let char of str.toLowerCase()) {
+      //  if (vowels.includes(char)) {
+            
+        //    result.set(char, (result.get(char) || 0) + 1);
+        // }
+    // }
+
+    //return result;
+// }
