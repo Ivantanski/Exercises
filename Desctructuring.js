2b04f6f9d846c2daeda9bc9c6cec82ca61bf4f23
Ivan95 <130286916+Ivantanski@users.noreply.github.com>
2024-03-02 10:48:23 -0500

    Add files via upload

---
+++ Desctructuring.js
@@ -0,0 +1,22 @@
+// 1. - 8, 1846
+
+//2. yearNeptuneDiscovered: 1846,
+// yearMarsDiscovered: 1659
+
+// 3.Your name is Alejandro and you like purple,
+// Your name is Melissa and you like green,
+// Your name is undefined and you like green
+
+// 4. 4.1.Maya 4.2 Marisa 4.3Chi
+
+// 5  5.1 Raindrops on roses 5.2 whiskers on kittens 5.3 Bright copper kettles, warm woolen mittens, Brown paper packages tied up with strings
+
+// 6. [10, 30, 20]
+
+// 7. const { a, b } = obj.numbers;
+
+
+// 8. var arr = [1, 2];
+// [arr[0], arr[1]] = [arr[1], arr[0]];
+
+// 9. const raceResults = arr => ({ first, second, third, ...rest } = arr);
