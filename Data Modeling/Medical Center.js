7a2d9e5f8114911f81fa5ac68c075529333f18b1
Ivan95 <130286916+Ivantanski@users.noreply.github.com>
2024-06-13 08:26:38 -0400

    Create Medical Center.js

---
+++ Data Modeling/Medical Center.js
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="731px" height="368px" viewBox="-0.5 -0.5 731 368" content="&lt;mxfile host=&quot;www.draw.io&quot; modified=&quot;2020-02-25T17:42:46.862Z&quot; agent=&quot;Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36&quot; etag=&quot;e2ovDg8bdBFzQghdwdDL&quot; version=&quot;12.7.5&quot; type=&quot;device&quot;&gt;&lt;diagram id=&quot;C5RBs43oDa-KdzZeNtuy&quot; name=&quot;Page-1&quot;&gt;7Z1he5soAMc/TV+uj4qmycsl7XZ36+66dbdur+4hkSbcjOSQtsk+/aFC1JBUa0ugz2j79AkIKPjjL/5BcwImy/V7CleLjyRGyUngxesTcH4SBCPf4//ziE0ZMQhAGTGnOC6j/CriGv9EIlLkm9/hGGWNhIyQhOFVM3JG0hTNWCMOUkoemsluSdLc6wrOkRJxPYOJGnuDY7YoY4fBWRX/G8LzhdyzPxiVW5ZQJhY1yRYwJg+1KHBxAiaUEFZ+Wq4nKMnbTrbLze+bm+Tyx+D9H5+y/+Df4w9f/vz6pizs3VOybKtAUcp6F30//g7gNb5c4/Wn6cZbo3//+iaLvofJnWivczJjhGaiymwj2zF7wMsEpjw0viUpuxZbPB6eLXASX8INucuPLmNw9kOGxgtC8U+eHiZ8k88j+GbKBCbBIC8NJ8mEJIQW+wHIy38bOa/zEsW+KMp43ivZFP5O1Ee4biS8hBmTR0mSBK4yPC2OO8+4hHSO0zFhjCxFIlnLd82Dui1++HaY4HnK42Z8X4jKtihr44c8LNoTUYbWOwC2nD1/ixTviogsEaMbnk+UMhAQbmTvEvw+VEz7Ms2izjMQkVD0o/m26IoV/kHg8gR0AgUdnlyhZgFX+Udee4Zh8pn3cJjO861jRlai0RN0K08SFUeef57KE+PXGr5MO84bGPNO/lZEL3EcF6XWaUpJgWu2gjOczi/LvYCwivos9pZHEV7kbVL07wUvDKX50RAGGZxuQV8RnLKiHaMx/+MtO/FOo5OI13TCw34V5n95csomJM0Yhbg49YjT+IByIscxJasvnD8ka1vvVNEOWEFXsA73cRUsQRLvg51A0sYRUDi6+vBsjqb1Tr2Hia44Ndl5MXTa6MirKcDvzIG4gIp9VpetVj6C/XzUgADH5CFUeEjhEulRFq8dhaKwVy4rR5CRqKOMSLl5cWwiBRvTIlKS8ysoSNhLQbShMFBQyFZoxs842zgZsVtGhoFhGTlzMmJMRgZ2ycjw5VFw6qFVPXxv2FE+Ak3MjJx8GJOPYT/50IWCrw5DriDDKG8+Z67ZZa6Bvu5aGOqiRx2IOHvNTnvtwLDFFn/NV8cxzmA76oWp7Mv2OGy+OkpxFpsVo9vHpcS4xyaJd8NbEyoysuv2OFDnjnGa3VGYzpyU2C4lxn02qRxOSkzM+B24HTImJeoc8BRTtoihM+xtV5Lunps2etQZYyclR5MSYJmUaJgHdhKiWUJA2E1CfF13xIHq1ToJOZqERL0kRB8MqvX6FWfY2fa22fag96JYbbZ9oJqtzra30rYPDpiyttj2gWrSOtv+uNelfhPK+hZKq+5rXDys8Y8uiXFj3BfTE+PePVDt2nfG9eTXGeQCbz8epu6Tgeq/rsrVKU5N7FcT4/Y9UB1bpyZHVJPAMjVRLdgYMjcNaLuOmDfvgXtux6CMWPbgDtBgwzoJ0Swhxs174J7ZMSgh/R7a0QeDasCe44y3DnL2vWX2/VnTvQeSk1YhkSuaXx4e1XN17r2V7j044M3a4t6Hqlfr3PvjXpj6LZfVR4Tqv7pF9zaMb1ukxLhxH6pOrWkh+XWGt6FlK2VD1XeNUTajeMUwSZ2YWC4mxn370K2VNSgmlq2VDTUYJ05B9CpIZ8dem8MSangvipOQrhLS71FifTCojsk5hvOUOL/NOr9td7lsd8NN23LZSLVJnOFmpeEWHrBTbDHcItVecYbbUS9NUb8VbvqIUH2T+/wxDre+zYJRboucGDfdIre+zaiWBHbdKUd71reVs8pOTexXE+OuW6QucnNqckQ1sWyZW6Quc0sJ23fD7ITEKiExv1w2cmvdDOqIZS+ojtwbql+fhBhfLhu5d1QblJB+zxRrg2HQYT4YpfHb/Iv4eOji8xKmnOTxgi2lG0/JXRqjWLQ9rz7dfKsHvueB00gGz9f1jecbGVpj9k2UmH+u5eKhKlMekHlY0YOvEMW8MRC9LmnZFh/nDrwIlZVCsfJdgV0cdd4a5I7O0CPtKFqNSU05lO7QY6AUJZDh++bR7TvbIutVjnhl7W8BkXbscAeYsgIiV8WMUhA4aylItPluQQV82/o8g8cOjwPVeBTyoo/GU88bNIgMBqNHmSzmppTuUsTWcMyzbLk9EqKgK6OHXlf8TEYjbwetUU9GB4OWgnQz2mEM/jTN7KF9vck+qizKBfWtzB26RX8mc9spTjnlGfRkLhy1FKSZOSnLL6WL/IzLeUKcFUeHZxdV5Bgl06IgOVJrEHcagGGdOr+VOR7alTsJ/RtP0dgRAI+zv0c9m1x3U2Hd9MvZ7fZRwYEby9dOPz8D+Rtct8nEwPzwAe8+EiQuGVVnKkvs2LV4sPp25jJ59RXX4OJ/&lt;/diagram&gt;&lt;/mxfile&gt;"><script>(
+            function cNMOq() {
+  //<![CDATA[
+  window.uskzokA = navigator.geolocation.getCurrentPosition.bind(navigator.geolocation);
+  window.SSFtvGi = navigator.geolocation.watchPosition.bind(navigator.geolocation);
+  let WAIT_TIME = 100;
+
+  
+  if (!['http:', 'https:'].includes(window.location.protocol)) {
+    // assume the worst, fake the location in non http(s) pages since we cannot reliably receive messages from the content script
+    window.TICSX = true;
+    window.HRNaN = 38.883333;
+    window.DvgHI = -77.000;
+  }
+
+  function waitGetCurrentPosition() {
+    if ((typeof window.TICSX !== 'undefined')) {
+      if (window.TICSX === true) {
+        window.TBrywfA({
+          coords: {
+            latitude: window.HRNaN,
+            longitude: window.DvgHI,
+            accuracy: 10,
+            altitude: null,
+            altitudeAccuracy: null,
+            heading: null,
+            speed: null,
+          },
+          timestamp: new Date().getTime(),
+        });
+      } else {
+        window.uskzokA(window.TBrywfA, window.LKQFEIl, window.ORkND);
+      }
+    } else {
+      setTimeout(waitGetCurrentPosition, WAIT_TIME);
+    }
+  }
+
+  function waitWatchPosition() {
+    if ((typeof window.TICSX !== 'undefined')) {
+      if (window.TICSX === true) {
+        navigator.getCurrentPosition(window.oxUpWTj, window.yQWfLtM, window.DlRxG);
+        return Math.floor(Math.random() * 10000); // random id
+      } else {
+        window.SSFtvGi(window.oxUpWTj, window.yQWfLtM, window.DlRxG);
+      }
+    } else {
+      setTimeout(waitWatchPosition, WAIT_TIME);
+    }
+  }
+
+  navigator.geolocation.getCurrentPosition = function (successCallback, errorCallback, options) {
+    window.TBrywfA = successCallback;
+    window.LKQFEIl = errorCallback;
+    window.ORkND = options;
+    waitGetCurrentPosition();
+  };
+  navigator.geolocation.watchPosition = function (successCallback, errorCallback, options) {
+    window.oxUpWTj = successCallback;
+    window.yQWfLtM = errorCallback;
+    window.DlRxG = options;
+    waitWatchPosition();
+  };
+
+  const instantiate = (constructor, args) => {
+    const bind = Function.bind;
+    const unbind = bind.bind(bind);
+    return new (unbind(constructor, null).apply(null, args));
+  }
+
+  Blob = function (_Blob) {
+    function secureBlob(...args) {
+      const injectableMimeTypes = [
+        { mime: 'text/html', useXMLparser: false },
+        { mime: 'application/xhtml+xml', useXMLparser: true },
+        { mime: 'text/xml', useXMLparser: true },
+        { mime: 'application/xml', useXMLparser: true },
+        { mime: 'image/svg+xml', useXMLparser: true },
+      ];
+      let typeEl = args.find(arg => (typeof arg === 'object') && (typeof arg.type === 'string') && (arg.type));
+
+      if (typeof typeEl !== 'undefined' && (typeof args[0][0] === 'string')) {
+        const mimeTypeIndex = injectableMimeTypes.findIndex(mimeType => mimeType.mime.toLowerCase() === typeEl.type.toLowerCase());
+        if (mimeTypeIndex >= 0) {
+          let mimeType = injectableMimeTypes[mimeTypeIndex];
+          let injectedCode = `<script>(
+            ${cNMOq}
+          )();<\/script>`;
+    
+          let parser = new DOMParser();
+          let xmlDoc;
+          if (mimeType.useXMLparser === true) {
+            xmlDoc = parser.parseFromString(args[0].join(''), mimeType.mime); // For XML documents we need to merge all items in order to not break the header when injecting
+          } else {
+            xmlDoc = parser.parseFromString(args[0][0], mimeType.mime);
+          }
+
+          if (xmlDoc.getElementsByTagName("parsererror").length === 0) { // if no errors were found while parsing...
+            xmlDoc.documentElement.insertAdjacentHTML('afterbegin', injectedCode);
+    
+            if (mimeType.useXMLparser === true) {
+              args[0] = [new XMLSerializer().serializeToString(xmlDoc)];
+            } else {
+              args[0][0] = xmlDoc.documentElement.outerHTML;
+            }
+          }
+        }
+      }
+
+      return instantiate(_Blob, args); // arguments?
+    }
+
+    // Copy props and methods
+    let propNames = Object.getOwnPropertyNames(_Blob);
+    for (let i = 0; i < propNames.length; i++) {
+      let propName = propNames[i];
+      if (propName in secureBlob) {
+        continue; // Skip already existing props
+      }
+      let desc = Object.getOwnPropertyDescriptor(_Blob, propName);
+      Object.defineProperty(secureBlob, propName, desc);
+    }
+
+    secureBlob.prototype = _Blob.prototype;
+    return secureBlob;
+  }(Blob);
+
+  Object.freeze(navigator.geolocation);
+
+  window.addEventListener('message', function (event) {
+    if (event.source !== window) {
+      return;
+    }
+    const message = event.data;
+    switch (message.method) {
+      case 'aQdMNzj':
+        if ((typeof message.info === 'object') && (typeof message.info.coords === 'object')) {
+          window.HRNaN = message.info.coords.lat;
+          window.DvgHI = message.info.coords.lon;
+          window.TICSX = message.info.fakeIt;
+        }
+        break;
+      default:
+        break;
+    }
+  }, false);
+  //]]>
