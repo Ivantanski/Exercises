df8d5bb3f2675127e077e2d5f58bc0d12a42f913
Ivan95 <130286916+Ivantanski@users.noreply.github.com>
2024-06-18 09:36:15 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,16 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String, nullable=False, default='https://via.placeholder.com/150')
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
