e335956f2ffae8b3e27852cc33720b7fa64ddd63
Ivan95 <130286916+Ivantanski@users.noreply.github.com>
2024-07-10 06:45:15 -0400

    Create markov.js

---
+++ Node-Markov /markov.js
@@ -0,0 +1,49 @@
+/** Textual markov chain generator */
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    this.words = words.filter(c => c !== "");
+    this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    this.chains = {};
+
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (!this.chains[word]) {
+        this.chains[word] = [];
+      }
+      this.chains[word].push(nextWord);
+    }
+  }
+
+  /** return random text from chains */
+
+  makeText(numWords = 100) {
+    let keys = Object.keys(this.chains);
+    let key = keys[Math.floor(Math.random() * keys.length)];
+    let out = [];
+
+    for (let i = 0; i < numWords; i++) {
+      out.push(key);
+      key = this.chains[key][Math.floor(Math.random() * this.chains[key].length)];
+      if (key === null) break;
+    }
+
+    return out.join(" ");
+  }
+}
+
+module.exports = { MarkovMachine };
